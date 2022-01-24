1220e25a1d8e03e92f30cae835e1ade3e8355c94
Akash Velagala <akash.velagala@zemosolabs.com>
2022-01-24 12:14:51 +0530

    regular expression modified

--- src/JavaAssignments/Assigment1.java
+++ src/JavaAssignments/Assigment1.java
@@ -5,19 +5,27 @@ import java.io.*;
 public class Assigment1 {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the text file you want to search");
-        String input=sc.nextLine();
-        File currentDirectory=new File(".");
-        File[] files=currentDirectory.listFiles();
-        String pattern=input+".txt";
-        if(files!=null) {
+        while(true) {
+            System.out.println("Enter the text file you want to search");
+            String input = sc.nextLine();
+            File currentDirectory = new File(".");
+            File[] files = currentDirectory.listFiles();
+            String pattern = input + ".txt";
+            boolean found=false;
             for (File file : files) {
                 if (Pattern.matches(pattern, file.getName())) {
-                    System.out.println("File Found! at location: "+file.getAbsolutePath());
-                    return;
+                    System.out.println("File Found! at location: " + file.getAbsolutePath());
+                    found=true;
+                    break;
                 }
             }
+            if(!found)
+            System.out.println("File not Found");
+
+            System.out.println("Enter q to exit, or any character to continue");
+            String temp=sc.nextLine();
+            if(temp.equals("q"))
+                break;
         }
-        System.out.println("File not Found");
     }
 }
